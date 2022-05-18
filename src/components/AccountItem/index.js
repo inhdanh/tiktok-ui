b608d0037bbbee78a4f267dbe6957bbae6bfb3ba
inhdanh <31440884+inhdanh@users.noreply.github.com>
2022-05-18 10:30:14 +0700

    Logic Header #1

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -3,13 +3,14 @@ import classNames from 'classnames/bind';
 import styles from './AccountItem.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import Image from '../Image';
 
 const cx = classNames.bind(styles);
 
 export default function AccountItem() {
   return (
     <div className={cx('wrapper')}>
-      <img className={cx('avatar')} src='' alt='Hoaa' />
+      <Image className={cx('avatar')} src='dsf' alt='Hoaa' />
       <div className={cx('info')}>
         <h4 className={cx('name')}>
           <span>Nguyen van a</span>
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,34 +1,25 @@
-import React, { useEffect, useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import classNames from 'classnames/bind';
-import styles from './Header.module.scss';
-import images from '~/assets/images';
 import {
-  faCircleXmark,
-  faSpinner,
-  faMagnifyingGlass,
-  faSignIn,
-  faEllipsisVertical,
-  faEarthAsia,
   faCircleQuestion,
-  faKeyboard,
-  faCloudUpload,
-  faMessage,
-  faUser,
   faCoins,
+  faEarthAsia,
+  faEllipsisVertical,
   faGear,
+  faKeyboard,
   faSignOut,
+  faUser,
 } from '@fortawesome/free-solid-svg-icons';
-import HeadlessTippy from '@tippyjs/react/headless';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Tippy from '@tippyjs/react';
+import classNames from 'classnames/bind';
+import React from 'react';
 import 'tippy.js/dist/tippy.css';
-
-import { Wrapper as PopperWrapper } from '~/components/Popper';
-import AccountItem from '~/components/AccountItem';
+import images from '~/assets/images';
 import Button from '~/components/Button';
-import Menu from '~/components/Popper/Menu';
-import { InboxIcon, MessageIcon, SearchIcon, UploadIcon } from '~/components/Icons';
+import { InboxIcon, MessageIcon, UploadIcon } from '~/components/Icons';
 import Image from '~/components/Image';
+import Menu from '~/components/Popper/Menu';
+import Search from '~/components/Search';
+import styles from './Header.module.scss';
 
 const cx = classNames.bind(styles);
 
@@ -49,14 +40,7 @@ const MENU_ITEMS = [
 ];
 
 export default function Header() {
-  const [searchResult, setSearchResult] = useState([]);
   const currentUser = true;
-
-  useEffect(() => {
-    setTimeout(() => {
-      setSearchResult([]);
-    }, 0);
-  }, []);
 
   const handleMenuChange = (menuItem) => {
     switch (menuItem.type) {
@@ -81,33 +65,7 @@ export default function Header() {
         <div className={cx('logo')}>
           <img src={images.logo} alt='TikTok' />
         </div>
-        <HeadlessTippy
-          interactive
-          visible={searchResult.length > 0}
-          render={(attrs) => (
-            <div className={cx('search-result')} tabIndex='-1' {...attrs}>
-              <PopperWrapper>
-                <h4 className={cx('search-title')}>Accounts</h4>
-                <AccountItem />
-                <AccountItem />
-                <AccountItem />
-              </PopperWrapper>
-            </div>
-          )}
-        >
-          <div className={cx('search')}>
-            <input placeholder='Search accounts and videos' spellCheck={false} />
-            <button className={cx('clear')}>
-              <FontAwesomeIcon icon={faCircleXmark} />
-            </button>
-            <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
-
-            <button className={cx('search-btn')}>
-              <SearchIcon />
-            </button>
-          </div>
-        </HeadlessTippy>
-
+        <Search />
         <div className={cx('actions')}>
           {currentUser ? (
             <>
